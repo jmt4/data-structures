07921d24c4150dc0a427a536c10a7020abf5c854
jim <jj@uranium.example.com>
2016-02-18 22:42:53 -0700

    commented header and source

--- bitvector/bitvector.c
+++ bitvector/bitvector.c
@@ -11,16 +11,18 @@ int allocate_vector(unsigned char** bv, int bits)
 
 void clear_bit(unsigned char bv[], int n)
 {
-	/* release pid from vector if found else nothing */
+	/* could add some more code here if we want to return a value that 
+	   indicates whether a bit was actually cleared vs clearing a bit 
+	   that is already zero'd
+	*/
 	bv[n >> BITSHIFT] = bv[n >> BITSHIFT] & ~(1 << (n & BITMASK));
 }
 
 int set_bit(unsigned char bv[], int n)
 {
-	/*calculate bucket by dividing by size of char (8 bits per byte)
-	  then we can set nth bit by shifting 1 n%BITMASK spaces and
-	  performing a BITWISE OR
-	*/
+	/* ANDing with BITMASK is equivalent to modulo bucket size 
+	 * Thus we can figure out which bit to set
+	 */
 	bv[n >> BITSHIFT] |= 1 << (n & BITMASK);
 	return 1;
 }
@@ -33,10 +35,6 @@ void destroy_vector(unsigned char** bv)
 
 int check_bit(unsigned char bv[], int n)
 {
-	/* figure out bucket with operations in index
-	   then we AND that value with 1 shifted to the
-	   spot we want to check. For example, test 3rd bit
-	   001100 & 1 << 3 = 001100 & 000100 = 0001000 != 0
-	*/
+	/* returns 1 if bit is present; 0 otherwise */
 	return bv[n >> BITSHIFT] & 1 << (n & BITMASK);
 }
--- bitvector/bitvector.h
+++ bitvector/bitvector.h
@@ -9,8 +9,21 @@
 #define BITMASK 0x7
 #endif
 
+/* Using an unsigned char* as the type for a bit vector. Seems to be the most "constant"
+   sized type among possible choices (int, for example).
+*/
+
+/* bv is the bitvector and it can keep track of n bits. it might have an extra bucket in some cases */
 int allocate_vector(unsigned char** bv, int bits);
+
+/* sets a particular bit n to 0 */
 void clear_bit(unsigned char bv[], int n);
+
+/* sets bit n to 1 */
 int set_bit(unsigned char bv[], int n);
+
+/* frees memory used by bitvector and sets bv to NULL */
 void destroy_vector(unsigned char** bv);
+
+/* checks if bit at position n is set. returns nonzero value if bit is there */
 int check_bit(unsigned char bv[], int n);
